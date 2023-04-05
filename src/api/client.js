1fbbf8293c5b681b304e8f8f369f321e856e95df
unknown <you@example.com>
2023-04-05 15:18:25 +0300

    changed base uel inside client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 
 //Axios is a promised-based HTTP client for JavaScript. It has the ability to make HTTP requests from the browser and handle the transformation of request and response data.
 import axios from "axios";
-const client = axios.create({baseURL:"http://localhost:8000/api"})
+const client = axios.create({baseURL:"https://best-courses-backend.onrender.com/api"})
 export default client;
